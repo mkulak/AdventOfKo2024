c74528dfd582f351b781fef47ead54c55d37e89c
mkulak <kleshney@gmail.com>
2024-12-05 17:51:56 +0100

    add day 5

--- src/main/kotlin/Day05.kt
+++ src/main/kotlin/Day05.kt
@@ -1,13 +1,10 @@
 fun main() {
     val lines = readInput("Day05")
     val ordering = HashMap<Int, MutableSet<Int>>()
-    lines.takeWhile { it.isNotEmpty() }.forEach {
-        val (a, b) = it.split("|")
+    lines.takeWhile { it.isNotEmpty() }.map { it.split("|") }.forEach { (a, b) ->
         ordering.getOrPut(b.toInt(), ::HashSet) += a.toInt()
     }
-    val updates = lines.takeLastWhile { it.isNotEmpty() }.map {
-        it.split(",").map(String::toInt)
-    }
+    val updates = lines.takeLastWhile { it.isNotEmpty() }.map { it.split(",").map(String::toInt) }
     val (valid, invalid) = updates.partition { isValid(it, ordering) }
     val part1 = valid.sumOf { it[it.size / 2] }
     val part2 = invalid.map { fix(it, ordering) }.sumOf { it[it.size / 2] }
@@ -18,26 +15,19 @@ fun main() {
 fun isValid(update: List<Int>, ordering: Map<Int, Set<Int>>): Boolean {
     val all = update.toSet()
     val seen = HashSet<Int>()
-    return update.all { u ->
-        val required = ordering[u] ?: emptySet()
-        seen += u
-        required.all { it in seen || it !in all }
+    return update.all { e ->
+        seen += e
+        ordering[e].orEmpty().all { it in seen || it !in all }
     }
 }
 
 fun fix(update: List<Int>, ordering: Map<Int, Set<Int>>): List<Int> {
     val all = update.toMutableSet()
-    val localOrdering = ordering.filterKeys { it in all }.mapValues { (_, required) -> required.filter { it in all } }
     val res = LinkedHashSet<Int>()
     while (all.isNotEmpty()) {
-        val e = all.find { e -> localOrdering[e].orEmpty().all { it in res } }!!
+        val e = all.find { e -> ordering[e].orEmpty().all { it in res || it !in all } }!!
         all -= e
         res += e
     }
     return res.toList()
 }
-
-//5248
-//4507
-
-
